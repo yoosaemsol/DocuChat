37b837699f91709ef8b2fe82d1e36557801e8dee
yoosaemsol <yoosaemsol@gmail.com>
2024-01-07 15:14:09 -0500

    feat: setup tRPC, setup prisma, sync db with auth-callback

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,6 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
 import { NextRequest } from 'next/server';
 
-// export const GET = handleAuth();
-
 export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <div>Hello World</div>;
+};
+
+export default Dashboard;
