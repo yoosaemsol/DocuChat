704f7517805bdde0beccd9d09e1c351654cf0953
yoosaemsol <yoosaemsol@gmail.com>
2024-01-08 01:27:51 -0500

    feat: implement dashboard - get files, delete file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
+import 'react-loading-skeleton/dist/skeleton.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
