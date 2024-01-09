c7e7a673772df521960e4acd8f074501147c931c
yoosaemsol <yoosaemsol@gmail.com>
2024-01-08 19:49:36 -0500

    feat: implement feature of dropping pdf file

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,6 +4,38 @@ import { useState } from 'react';
 import { Button } from './ui/button';
 import { Dialog, DialogContent, DialogTrigger } from './ui/dialog';
 
+import Dropzone from 'react-dropzone';
+
+const UploadDropzone = () => {
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={(acceptedFile) => {
+        console.log('acceptedFile');
+        console.log(acceptedFile);
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"
+        >
+          <div className="flex items-center justify-center h-full w-full">
+            <label
+              htmlFor="dropzone-file"
+              className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+            >
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                example
+              </div>
+            </label>
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
+
 const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -20,7 +52,9 @@ const UploadButton = () => {
         <Button>Upload PDF</Button>
       </DialogTrigger>
 
-      <DialogContent>example content</DialogContent>
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 };
